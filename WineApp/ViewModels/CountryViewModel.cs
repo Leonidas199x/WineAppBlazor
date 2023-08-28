3bd567df779f47edd7fc1f0b5d7f9944be784c26
Leonidas199x <toby.w.prince@gmail.com>
2023-08-28 13:16:28 +0100

    Addition of CountryViewModel

---
+++ WineApp/ViewModels/CountryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class CountryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string  Name { get; set; } = string.Empty;
+
+        [Required]
+        public string IsoCode { get; set; } = string.Empty;
+
+        public string Note { get; set; } = string.Empty;
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    }
+}
