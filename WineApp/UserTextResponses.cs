7bdfd3fc8d805bfab7c860c486c9168bffdc3eb6
Leonidas199x <toby.w.prince@gmail.com>
2023-08-28 13:16:42 +0100

    Addition of UserTextResponses

---
+++ WineApp/UserTextResponses.cs
@@ -0,0 +1,7 @@
+﻿namespace WineApp
+{
+    public static class UserTextResponses
+    {
+        public static string ErrorMessage = "Sorry, something went wrong. Please try again. If the issue persists, please tell Toby he is an idiot";
+    }
+}
