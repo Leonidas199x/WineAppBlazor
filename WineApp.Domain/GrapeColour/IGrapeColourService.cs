f0dcd9ad9fcfc6d401c7fac641f2cee2364f7668
Leonidas199x <toby.w.prince@gmail.com>
2023-08-29 20:12:02 +0100

    Addition of grape colour service

---
+++ WineApp.Domain/GrapeColour/IGrapeColourService.cs
@@ -0,0 +1,19 @@
+﻿using DataContract;
+
+namespace WineApp.Domain.GrapeColour
+{
+    public interface IGrapeColourService
+    {
+        Task<Result<PagedList<IEnumerable<DataContract.GrapeColour>>>> GetAll(int page, int pageSize);
+
+        Task<Result<PagedList<IEnumerable<DataContract.GrapeColour>>>> Search(string colour, int page, int pageSize);
+
+        Task<Result<DataContract.GrapeColour>> Get(int id);
+
+        Task<Result> Put(DataContract.GrapeColour grapeColour);
+
+        Task<Result> Post(DataContract.GrapeColour grapeColour);
+
+        Task<Result> Delete(int id);
+    }
+}
