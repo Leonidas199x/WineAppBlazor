1bd425b6a31ba3b08e4955823df39583bc1dd8de
Leonidas199x <toby.w.prince@gmail.com>
2023-08-29 20:11:51 +0100

    Addition of Grape Colour mapper

---
+++ WineApp/Mappers/GrapeColourMapper.cs
@@ -0,0 +1,26 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public class GrapeColourMapper : IGrapeColourMapper
+    {
+        public GrapeColour Map(GrapeColourViewModel value)
+        {
+            return new GrapeColour
+            {
+                Id = value.Id,
+                Colour = value.Colour,
+            };
+        }
+
+        public GrapeColourViewModel Map(GrapeColour value)
+        {
+            return new GrapeColourViewModel
+            {
+                Id = value.Id,
+                Colour = value.Colour,
+            };
+        }
+    }
+}
---
+++ WineApp/Mappers/IGrapeColourMapper.cs
@@ -0,0 +1,12 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public interface IGrapeColourMapper
+    {
+        GrapeColour Map(GrapeColourViewModel value);
+
+        GrapeColourViewModel Map(GrapeColour value);
+    }
+}
