213cd87a2f68bd480f743315f2bafa5dcc496d2e
Leonidas199x <toby.w.prince@gmail.com>
2023-08-29 20:12:17 +0100

    Addition of Grape Colour viewmodel

---
+++ WineApp/ViewModels/GrapeColourViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class GrapeColourViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Colour { get; set; } = string.Empty;
+    }
+}
