b36c1f6f08f1c52f90e6e70a6bbe0123537cfeb8
Leonidas199x <toby.w.prince@gmail.com>
2023-08-30 22:45:06 +0100

    Addition of stopper type mapper

---
+++ WineApp/Mappers/IStopperTypeMapper.cs
@@ -0,0 +1,14 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public interface IStopperTypeMapper
+    {
+        StopperType Map(StopperTypeViewModel value);
+
+        StopperTypeViewModel Map(StopperType value);
+
+        StopperTypeCreate MapNew(StopperTypeViewModel value);
+    }
+}
---
+++ WineApp/Mappers/StopperTypeMapper.cs
@@ -0,0 +1,34 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public class StopperTypeMapper : IStopperTypeMapper
+    {
+        public StopperType Map(StopperTypeViewModel value)
+        {
+            return new StopperType
+            {
+                Id = value.Id,
+                Name = value.Name,
+            };
+        }
+
+        public StopperTypeViewModel Map(StopperType value)
+        {
+            return new StopperTypeViewModel
+            {
+                Id = value.Id,
+                Name = value.Name,
+            };
+        }
+
+        public StopperTypeCreate MapNew(StopperTypeViewModel value)
+        {
+            return new StopperTypeCreate
+            {
+                Name = value.Name,
+            };
+        }
+    }
+}
