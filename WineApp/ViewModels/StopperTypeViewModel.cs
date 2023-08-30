06421792bf1a120da5971bfb823abf65b798947a
Leonidas199x <toby.w.prince@gmail.com>
2023-08-30 22:45:31 +0100

    Addition fo stopper type view model

---
+++ WineApp/ViewModels/StopperTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class StopperTypeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+    }
+}
