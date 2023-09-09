eb86addd93ee782bcb278b391d59ff3be9b18bc1
Leonidas199x <toby.w.prince@gmail.com>
2023-09-09 19:52:42 +0100

    Addition of retailer mappers

---
+++ WineApp/Mappers/IRetailerMapper.cs
@@ -0,0 +1,14 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public interface IRetailerMapper
+    {
+        Retailer Map(RetailerViewModel value);
+
+        RetailerViewModel Map(Retailer value);
+
+        RetailerCreate MapNew(RetailerViewModel value);
+    }
+}
---
+++ WineApp/Mappers/RetailerMapper.cs
@@ -0,0 +1,68 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public class RetailerMapper : IRetailerMapper
+    {
+        public Retailer Map(RetailerViewModel value)
+        {
+            return new Retailer
+            {
+                Id = value.Id,
+                Name = value.Name,
+                MinimumPurchaseQuantity = value.MinimumPurchaseQuantity,
+                IncrementQuantity = value.IncrementQuantity,
+                GenericDiscountPercentage = value.GenericDiscountPercentage,
+                GenericDiscountName = value.GenericDiscountName,
+                WebsiteUrl = value.WebsiteUrl.ToString(),
+                WebsiteRating = value.WebsiteRating,
+                OrderRating = value.OrderRating,
+                DeliveryRating = value.DeliveryRating,
+                MaxCustomerRating = value.MaxCustomerRating,
+                Note = value.Note,
+                DateCreated = value.DateCreated,
+                DateUpdated = value.DateUpdated,
+            };
+        }
+
+        public RetailerViewModel Map(Retailer value)
+        {
+            return new RetailerViewModel
+            {
+                Id = value.Id,
+                Name = value.Name,
+                MinimumPurchaseQuantity = value.MinimumPurchaseQuantity,
+                IncrementQuantity = value.IncrementQuantity,
+                GenericDiscountPercentage = value.GenericDiscountPercentage,
+                GenericDiscountName = value.GenericDiscountName,
+                WebsiteUrl = new Uri(value.WebsiteUrl),
+                WebsiteRating = value.WebsiteRating,
+                OrderRating = value.OrderRating,
+                DeliveryRating = value.DeliveryRating,
+                MaxCustomerRating = value.MaxCustomerRating,
+                Note = value.Note,
+                DateCreated = value.DateCreated,
+                DateUpdated = value.DateUpdated,
+            };
+        }
+
+        public RetailerCreate MapNew(RetailerViewModel value)
+        {
+            return new RetailerCreate
+            {
+                Name = value.Name,
+                MinimumPurchaseQuantity = value.MinimumPurchaseQuantity,
+                IncrementQuantity = value.IncrementQuantity,
+                GenericDiscountPercentage = value.GenericDiscountPercentage,
+                GenericDiscountName = value.GenericDiscountName,
+                WebsiteUrl = value.WebsiteUrl.ToString(),
+                WebsiteRating = value.WebsiteRating,
+                OrderRating = value.OrderRating,
+                DeliveryRating = value.DeliveryRating,
+                MaxCustomerRating = value.MaxCustomerRating,
+                Note = value.Note,
+            };
+        }
+    }
+}
