3dc5356ec115dc54d93d115a36623e0a4da683e8
Leonidas199x <toby.w.prince@gmail.com>
2023-09-09 19:52:15 +0100

    Addition of retailer service

---
+++ WineApp.Domain/Retailer/IRetailerService.cs
@@ -0,0 +1,19 @@
+﻿using DataContract;
+
+namespace WineApp.Domain.Retailer
+{
+    public interface IRetailerService
+    {
+        Task<Result<PagedList<IEnumerable<DataContract.Retailer>>>> GetAll(int page, int pageSize);
+
+        Task<Result<PagedList<IEnumerable<DataContract.Retailer>>>> Search(string colour, int page, int pageSize);
+
+        Task<Result<DataContract.Retailer>> Get(int id);
+
+        Task<Result> Put(DataContract.Retailer retailer);
+
+        Task<Result> Post(RetailerCreate retailer);
+
+        Task<Result> Delete(int id);
+    }
+}
