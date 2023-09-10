9eb21ef3ff9bc3757166844198b965d016d57e80
Leonidas199x <toby.w.prince@gmail.com>
2023-09-10 17:39:01 +0100

    Addition of region mapper

---
+++ WineApp/Mappers/IRegionMapper.cs
@@ -0,0 +1,14 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public interface IRegionMapper
+    {
+        Region Map(RegionViewModel value);
+
+        RegionViewModel Map(Region value);
+
+        RegionCreate MapNew(RegionViewModel value);
+    }
+}
---
+++ WineApp/Mappers/RegionMapper.cs
@@ -0,0 +1,60 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public class RegionMapper : IRegionMapper
+    {
+        private ICountryMapper _countryMapper;
+
+        public RegionMapper(ICountryMapper countryMapper)
+        {
+            _countryMapper = countryMapper;
+        }
+
+        public Region Map(RegionViewModel value)
+        {
+            return new Region
+            {
+                Id = value.Id,
+                Name = value.Name,
+                IsoCode = value.IsoCode,
+                Note = value.Note,
+                Longitude = value.Longitude,
+                Latitude = value.Latitude,
+                DateCreated = value.DateCreated,
+                DateUpdated = value.DateUpdated,
+                Country = _countryMapper.Map(value.Country),
+            };
+        }
+
+        public RegionViewModel Map(Region value)
+        {
+            return new RegionViewModel
+            {
+                Id = value.Id,
+                Name = value.Name,
+                IsoCode = value.IsoCode,
+                Note = value.Note,
+                Longitude = value.Longitude,
+                Latitude = value.Latitude,
+                DateCreated = value.DateCreated,
+                DateUpdated = value.DateUpdated,
+                Country = _countryMapper.Map(value.Country),
+            };
+        }
+
+        public RegionCreate MapNew(RegionViewModel value)
+        {
+            return new RegionCreate
+            {
+                Name = value.Name,
+                IsoCode = value.IsoCode,
+                Note = value.Note,
+                Longitude = value.Longitude,
+                Latitude = value.Latitude,
+                CountryId = value.Country.Id,
+            };
+        }
+    }
+}
