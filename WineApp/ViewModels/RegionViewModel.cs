2196a23efa05cad71408a8f79f12a69c0a19f365
Leonidas199x <toby.w.prince@gmail.com>
2023-09-10 17:39:11 +0100

    Addition of region viewmodel

---
+++ WineApp/ViewModels/RegionViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace WineApp.ViewModels
+{
+    public class RegionViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string IsoCode { get; set; } = string.Empty;
+
+        public CountryViewModel Country { get; set; }
+
+        public string Note { get; set; } = string.Empty;
+
+        public decimal? Longitude { get; set; }
+
+        public decimal? Latitude { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    }
+}
