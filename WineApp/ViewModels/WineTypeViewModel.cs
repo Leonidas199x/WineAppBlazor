6e26382e7f5ec224aa9264e64e740215821715b9
Leonidas199x <toby.w.prince@gmail.com>
2023-09-16 17:05:26 +0100

    Addition fo wine type viewmodel

---
+++ WineApp/ViewModels/WineTypeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class WineTypeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Note { get; set; } = string.Empty;
+    }
+}
