04e10e184ac15dcd7aa4afd2925991a5afca8881
Leonidas199x <toby.w.prince@gmail.com>
2023-09-16 17:05:04 +0100

    Addition of wine type service

---
+++ WineApp.Domain/WineType/IWineTypeService.cs
@@ -0,0 +1,19 @@
+﻿using DataContract;
+
+namespace WineApp.Domain.WineType
+{
+    public interface IWineTypeService
+    {
+        Task<Result<PagedList<IEnumerable<DataContract.WineType>>>> GetAll(int page, int pageSize);
+
+        Task<Result<PagedList<IEnumerable<DataContract.WineType>>>> Search(string name, int page, int pageSize);
+
+        Task<Result<DataContract.WineType>> Get(int id);
+
+        Task<Result> Put(DataContract.WineType vineywarEstate);
+
+        Task<Result> Post(WineTypeCreate vineyardEstate);
+
+        Task<Result> Delete(int id);
+    }
+}
