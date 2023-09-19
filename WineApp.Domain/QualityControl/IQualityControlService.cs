4d91c302eb9c5bf1c6967d7c1647bac0c4f5a216
Leonidas199x <toby.w.prince@gmail.com>
2023-09-19 10:08:43 +0100

    Addition of Quality Control service

---
+++ WineApp.Domain/QualityControl/IQualityControlService.cs
@@ -0,0 +1,19 @@
+﻿using DataContract;
+
+namespace WineApp.Domain.QualityControl
+{
+    public interface IQualityControlService
+    {
+        Task<Result<PagedList<IEnumerable<DataContract.QualityControl>>>> GetAll(int page, int pageSize);
+
+        Task<Result<PagedList<IEnumerable<DataContract.QualityControl>>>> Search(string name, int page, int pageSize);
+
+        Task<Result<DataContract.QualityControl>> Get(int id);
+
+        Task<Result> Put(DataContract.QualityControl qualityControl);
+
+        Task<Result> Post(QualityControlCreate qualityControl);
+
+        Task<Result> Delete(int id);
+    }
+}
---
+++ WineApp.Domain/QualityControl/QualityControlService.cs
@@ -0,0 +1,70 @@
+﻿using DataContract;
+using Newtonsoft.Json;
+
+namespace WineApp.Domain.QualityControl
+{
+    public class QualityControlService : IQualityControlService
+    {
+        private readonly string _endpoint = "QualityControl";
+        private readonly IHttpRequestHandler _request;
+
+        public QualityControlService(IHttpRequestHandler request)
+        {
+            _request = request;
+        }
+
+        public async Task<Result<PagedList<IEnumerable<DataContract.QualityControl>>>> GetAll(int page, int pageSize)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_endpoint}?page={page}&pageSize={pageSize}");
+
+            return await _request
+                .SendAsync<PagedList<IEnumerable<DataContract.QualityControl>>>(request)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Result<PagedList<IEnumerable<DataContract.QualityControl>>>> Search(string name, int page, int pageSize)
+        {
+            var url = $"{_endpoint}/search?name={name}&page={page}&pageSize={pageSize}";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            return await _request
+                .SendAsync<PagedList<IEnumerable<DataContract.QualityControl>>>(request)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Result<DataContract.QualityControl>> Get(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_endpoint}/{id}");
+
+            return await _request
+                .SendAsync<DataContract.QualityControl>(request)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Result> Put(DataContract.QualityControl qualityControl)
+        {
+            var json = JsonConvert.SerializeObject(qualityControl);
+            return await _request
+                    .PutAsync(_endpoint, json)
+                    .ConfigureAwait(false);
+        }
+
+        public async Task<Result> Post(QualityControlCreate qualityControl)
+        {
+            var json = JsonConvert.SerializeObject(qualityControl);
+
+            return await _request
+                    .PostAsync(_endpoint, json)
+                    .ConfigureAwait(false);
+        }
+
+        public async Task<Result> Delete(int id)
+        {
+            var url = $"{_endpoint}/{id}";
+
+            return await _request
+                        .DeleteAsync(url)
+                        .ConfigureAwait(false);
+        }
+    }
+}
