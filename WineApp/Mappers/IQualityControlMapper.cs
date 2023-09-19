98d71b7b4c75b882befaa11e236e99949dbd902b
Leonidas199x <toby.w.prince@gmail.com>
2023-09-19 10:45:21 +0100

    Addition of quality control mappers

---
+++ WineApp/Mappers/IQualityControlMapper.cs
@@ -0,0 +1,14 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public interface IQualityControlMapper
+    {
+        QualityControl Map(QualityControlViewModel value);
+
+        QualityControlViewModel Map(QualityControl value);
+
+        QualityControlCreate MapNew(QualityControlViewModel value);
+    }
+}
---
+++ WineApp/Mappers/QualityControlMapper.cs
@@ -0,0 +1,47 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public class QualityControlMapper : IQualityControlMapper
+    {
+        private readonly ICountryMapper _countryMapper;
+
+        public QualityControlMapper(ICountryMapper countryMapper)
+        {
+            _countryMapper = countryMapper;
+        }
+
+        public QualityControl Map(QualityControlViewModel value)
+        {
+            return new QualityControl
+            {
+                Id = value.Id,
+                Name = value.Name,
+                Country = _countryMapper.Map(value.Country),
+                Note = value.Note,
+            };
+        }
+
+        public QualityControlViewModel Map(QualityControl value)
+        {
+            return new QualityControlViewModel
+            {
+                Id = value.Id,
+                Name = value.Name,
+                Country = _countryMapper.Map(value.Country),
+                Note = value.Note,
+            };
+        }
+
+        public QualityControlCreate MapNew(QualityControlViewModel value)
+        {
+            return new QualityControlCreate
+            {
+                Name = value.Name,
+                Note = value.Note,
+                CountryId = value.Country.Id,
+            };
+        }
+    }
+}
