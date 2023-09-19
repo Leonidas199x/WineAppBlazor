a4f4be8cf4033f4de047cb26e512be9e0ebd80fa
Leonidas199x <toby.w.prince@gmail.com>
2023-09-19 17:10:53 +0100

    Addition of RatingViewModel

---
+++ WineApp/ViewModels/RatingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class RatingViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int DrinkerId { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+    }
+}
