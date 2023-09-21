d701ce1fb47e3b02642111794921964a9d805788
Leonidas199x <toby.w.prince@gmail.com>
2023-09-21 19:25:25 +0100

    Addition of rating mapper

---
+++ WineApp/Mappers/RatingMapper.cs
@@ -0,0 +1,33 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public static class RatingMapper
+    {
+        public static RatingViewModel Map(WineRating value)
+        {
+            return new RatingViewModel
+            {
+                Id = value.Id,
+                DrinkerId = value.Drinker.Id,
+                WineId = value.WineId,
+                Rating = value.Rating
+            };
+        }
+
+        public static WineRating Map(RatingViewModel value)
+        {
+            return new WineRating
+            {
+                Id = value.Id,
+                WineId = value.WineId,
+                Rating = value.Rating,
+                Drinker = new Drinker
+                {
+                    Id = value.DrinkerId,
+                },
+            };
+        }
+    }
+}
