84d11ef3e38439e4a6199e0403cf84f858ed4e73
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 21:55:38 +0100

    Addition of Issue Mapper

---
+++ WineApp/Mappers/IssueMapper.cs
@@ -0,0 +1,32 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public static class IssueMapper
+    {
+        public static IssueViewModel Map(Issue value)
+        {
+            return new IssueViewModel
+            {
+                Id = value.Id,
+                WineId = value.WineId,
+                Date = value.Date,
+                Quantity = value.Quantity,
+                Note = value.Note,
+            };
+        }
+
+        public static Issue Map(IssueViewModel value)
+        {
+            return new Issue
+            {
+                Id = value.Id,
+                WineId = value.WineId,
+                Date = value.Date,
+                Quantity = value.Quantity,
+                Note = value.Note,
+            };
+        }
+    }
+}
