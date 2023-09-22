de1df99e78c584e57b08b22f58de921080cf6a0d
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 21:55:54 +0100

    Addition of view model

---
+++ WineApp/ViewModels/IssueViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class IssueViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int WineId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Note { get; set; } = string.Empty;
+    }
+}
