bcf959f99d861377d10a7bf2b3d201a63c93fd76
Leonidas199x <toby.w.prince@gmail.com>
2023-09-22 18:40:06 +0100

    Addition of Issue service

---
+++ WineApp.Domain/Issue/IIssueService.cs
@@ -0,0 +1,13 @@
+﻿namespace WineApp.Domain.Issue
+{
+    public interface IIssueService
+    {
+        Task<Result<IEnumerable<DataContract.Issue>>> GetByWineId(int wineId);
+
+        Task<Result> Put(DataContract.Issue issue);
+
+        Task<Result> Post(DataContract.Issue issue);
+
+        Task<Result> Delete(int id);
+    }
+}
---
+++ WineApp.Domain/Issue/IssueService.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+
+namespace WineApp.Domain.Issue
+{
+    public class IssueService : IIssueService
+    {
+        private readonly string _endpoint = "Issue";
+        private readonly IHttpRequestHandler _request;
+
+        public IssueService(IHttpRequestHandler request)
+        {
+            _request = request;
+        }
+
+        public async Task<Result<IEnumerable<DataContract.Issue>>> GetByWineId(int wineId)
+        {
+            var url = $"{_endpoint}/WineId/{wineId}";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            return await _request
+                            .SendAsync<IEnumerable<DataContract.Issue>>(request)
+                            .ConfigureAwait(false);
+        }
+
+        public async Task<Result> Put(DataContract.Issue issue)
+        {
+            var json = JsonConvert.SerializeObject(issue);
+            return await _request
+                    .PutAsync(_endpoint, json)
+                    .ConfigureAwait(false);
+        }
+
+        public async Task<Result> Post(DataContract.Issue issue)
+        {
+            var json = JsonConvert.SerializeObject(issue);
+
+            return await _request
+                    .PostAsync(_endpoint, json)
+                    .ConfigureAwait(false);
+        }
+
+        public async Task<Result> Delete(int id)
+        {
+            var url = $"{_endpoint}/{id}";
+
+            return await _request
+                        .DeleteAsync(url)
+                        .ConfigureAwait(false);
+        }
+    }
+}
