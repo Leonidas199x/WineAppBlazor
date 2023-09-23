5ce8578a590e230b9274202f8abc65f1f73b8da4
Leonidas199x <toby.w.prince@gmail.com>
2023-09-23 16:02:42 +0100

    Addition of WineViewModel

---
+++ WineApp/ViewModels/WineViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class WineViewModel
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public int? ProducerId { get; set; }
+
+        public int? RegionId { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Vintage must be between 1900 & 2100")]
+        public int Vintage { get; set; }
+
+        public int? QualityControlId { get; set; }
+
+        public int? VineyardEstateId { get; set; }
+
+        public int? WineTypeId { get; set; }
+
+        public decimal Abv { get; set; }
+
+        public string Importer { get; set; } = string.Empty;
+
+        public int InventoryLevel { get; set; }
+
+        public int? ExclusiveRetailerId { get; set; }
+    }
+}
