a08d715d8418e45ca547d78a3f50348ea28abd47
Leonidas199x <toby.w.prince@gmail.com>
2023-09-23 16:02:51 +0100

    Addition of Wine Mapper

---
+++ WineApp/Mappers/WineMapper.cs
@@ -0,0 +1,26 @@
+﻿using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public static class WineMapper
+    {
+        public static WineViewModel Map(DataContract.Wine value)
+        {
+            return new WineViewModel
+            {
+                Id = value.Id,
+                Description = value.Description,
+                Vintage = value.Vintage,
+                Abv = value.Abv,
+                Importer = value.Importer,
+                InventoryLevel = value.InventoryLevel,
+                ExclusiveRetailerId = value.ExclusiveRetailer?.Id,
+                ProducerId = value.Producer?.Id,
+                RegionId = value.Region?.Id,
+                QualityControlId = value.QualityControl?.Id,
+                VineyardEstateId = value.VineyardEstate?.Id,
+                WineTypeId = value.WineType?.Id,
+            };
+        }
+    }
+}
