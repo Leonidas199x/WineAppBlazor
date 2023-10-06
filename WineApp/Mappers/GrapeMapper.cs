63452ce7e26a6a83e0daed597e931fb657c193d0
Toby Prince <toby.w.prince@outlook.com>
2023-10-06 17:39:04 +0100

    Addition of grape mapper

---
+++ WineApp/Mappers/GrapeMapper.cs
@@ -0,0 +1,43 @@
+﻿using WineApp.ViewModels;
+using DataContract;
+
+namespace WineApp.Mappers
+{
+    public static class GrapeMapper
+    {
+        public static GrapeViewModel Map(Grape value)
+        {
+            return new GrapeViewModel
+            {
+                Id = value.Id,
+                Name = value.Name,
+                GrapeColourId = value.Colour.Id,
+                Note = value.Note,
+            };
+        }
+
+        public static GrapeCreate MapNew(GrapeViewModel value)
+        {
+            return new GrapeCreate
+            {
+                Name = value.Name,
+                GrapeColourId = value.GrapeColourId,
+                Note = value.Note,
+            };
+        }
+
+        public static Grape Map(GrapeViewModel value) 
+        {
+            return new Grape
+            {
+                Id = value.Id,
+                Name = value.Name,
+                Colour = new GrapeColour
+                {
+                    Id = value.GrapeColourId.Value,
+                },
+                Note = value.Note,
+            };
+        }
+    }
+}
