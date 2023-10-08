6fa91c7a3090e9964687f62b1c22403c4357fc4c
Toby Prince <toby.w.prince@outlook.com>
2023-10-08 15:58:37 +0100

    Addition of WineGrapeViewModel

---
+++ WineApp/ViewModels/WineGrapeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WineApp.ViewModels
+{
+    public class WineGrapeViewModel
+    {
+        public int Id { get; set; }
+
+        public int WineId { get; set; }
+
+        [Required]
+        public int GrapeId { get; set; }
+    }
+}
