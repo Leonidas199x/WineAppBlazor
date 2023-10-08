202f0c46d276a37bc3b1d9dcf7c6a517ecd42837
Toby Prince <toby.w.prince@outlook.com>
2023-10-08 15:58:25 +0100

    Addition of map

---
+++ WineApp/Mappers/WineGrapeMapper.cs
@@ -0,0 +1,18 @@
+﻿using DataContract;
+using WineApp.ViewModels;
+
+namespace WineApp.Mappers
+{
+    public static class WineGrapeMapper
+    {
+        public static WineGrape Map(WineGrapeViewModel value)
+        {
+            return new WineGrape
+            {
+                Id = value.Id,
+                GrapeId = value.GrapeId,
+                WineId = value.WineId,
+            };
+        }
+    }
+}
